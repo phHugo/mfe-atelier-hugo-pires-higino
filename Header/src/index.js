2b825902009b1c8319d3f9cee741f32faa92e65a
phHugo <pireshigino.hugo31@gmail.com>
2025-03-03 16:01:51 +0100

    H de header warning

--- Header/src/index.js
+++ Header/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from './header';
+import Header from './Header';
 
 const mount = () => {
   const container = document.getElementById('root');
